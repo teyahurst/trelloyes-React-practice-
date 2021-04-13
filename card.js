3cca7a650352c6cbfad055e335cbb78e63ce9ac0
Teya Hurst <67305351+teyahurst@users.noreply.github.com>
2021-04-13 11:05:43 -0500

    Add files via upload

---
+++ card.js
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import './card.css';
+
+export default function Card(props){
+    return (
+        <div className='Card'>
+            <button type="button">delete</button>
+            <h3>{props.title}</h3>
+            <p>{props.content}</p>
+        </div>
+    )
+
+};
---
+++ list.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from './card.js';
+import './list.css';
+
+export default function List(props){
+    return (
+        <section className='List'>
+            <header className='List-header'>
+                <h2>{props.header}</h2>
+            </header>
+            <div className='List-cards'>
+                {props.cards.map((card) =>
+                    <Card key={card.key}
+                          title={card.title} 
+                          content={card.content}/>
+                          
+                )}
+
+                <button type="button" className="List-add-button">+ Add Random Card</button>
+            
+            </div>
+        </section>
+    )
+}
